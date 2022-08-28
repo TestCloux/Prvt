319cb20dec9fb39d4ade38a94c1a2482ee91238b
PorampokkuX <111211051+PorampokkuX@users.noreply.github.com>
2022-08-28 22:01:37 +0530

    Forgoted!

--- bot/__main__.py
+++ bot/__main__.py
@@ -24,7 +24,8 @@ from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 from bot.modules.wayback import getRandomUserAgent
 from .modules import authorize, list, cancel_mirror, mirror_status, mirror_leech, clone, ytdlp, shell, eval, \
-                    delete, count, leech_settings, search, rss, wayback, speedtest, usage, anilist, bt_select, mediainfo, hash, sleep
+
+                    delete, count, leech_settings, search, rss, wayback, speedtest, usage, anilist, bt_select, mediainfo, hash, sleep, addons
 from bot import *
 from datetime import datetime
 
